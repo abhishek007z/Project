7dad31b3bdbdc0b460a776bebc00bd30c1e0685d
abhishek007z <pandeyyabhi001@gmail.com>
2026-05-01 20:59:38 +0530

    authMiddleware error correction

--- Backend/Backend/middlewares/authMiddleware.js
+++ Backend/Backend/middlewares/authMiddleware.js
@@ -13,6 +13,7 @@ const protect = (req, res, next) => {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    
     req.userId = decoded.userId;
     req.user = { _id: decoded.userId }; // Set req.user for compatibility
     next();
